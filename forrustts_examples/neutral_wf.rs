aa3fe72d32175bcabfb4cf38cb1e2a1aa0f170ae
molpopgen <krthornt@uci.edu>
2020-12-15 13:29:45 -0800

    Example program now writes to file. Allow program to dump unsimplified trees

--- forrustts_examples/neutral_wf.rs
+++ forrustts_examples/neutral_wf.rs
@@ -1,5 +1,6 @@
 use clap::{value_t, value_t_or_exit, App, Arg};
 use forrustts::wright_fisher::neutral_wf;
+use tskit_rust;
 
 fn main() {
     let matches = App::new("neutral_wf")
@@ -31,6 +32,13 @@ fn main() {
                 .takes_value(true),
         )
         .arg(
+            Arg::with_name("outfile")
+                .short("o")
+                .long("outfile")
+                .help("Name of output file. The format is a tskit \"trees\" file")
+                .takes_value(true),
+        )
+        .arg(
             Arg::with_name("seed")
                 .short("S")
                 .long("seed")
@@ -44,6 +52,7 @@ fn main() {
     let rho = value_t_or_exit!(matches.value_of("rho"), f64);
     let simplify_input = value_t!(matches.value_of("simplification_interval"), i64).unwrap_or(-1);
     let seed = value_t_or_exit!(matches.value_of("seed"), usize);
+    let outfile = value_t_or_exit!(matches.value_of("outfile"), String);
 
     let mut simplify: Option<i64> = None;
 
@@ -53,5 +62,27 @@ fn main() {
 
     let r = rho / (4.0 * N as f64);
 
-    let tables = neutral_wf(seed, N, g, 10000000, r, 0.0, simplify);
+    let mut tables = neutral_wf(seed, N, g, 10000000, r, 0.0, simplify);
+
+    let mut is_sample = vec![0 as i32; tables.num_nodes()];
+    for (i, n) in tables.enumerate_nodes() {
+        if n.time == g {
+            is_sample[i] = 1;
+        }
+    }
+
+    let mut tskit_tables = forrustts::tskit::convert_to_tskit_and_drain(
+        &is_sample,
+        forrustts::tskit::simple_time_reverser(g),
+        if simplify.is_some() { true } else { false },
+        &mut tables,
+    );
+
+    if simplify.is_some() {
+        tskit_tables.dump(&outfile, 0).unwrap();
+    } else {
+        tskit_tables
+            .dump(&outfile, tskit_rust::TSK_NO_BUILD_INDEXES)
+            .unwrap();
+    }
 }
