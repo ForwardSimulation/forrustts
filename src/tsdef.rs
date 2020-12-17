b7e17a6edb8518e2814f40a7fd6d1bdbe98b99a9
molpopgen <krthornt@uci.edu>
2020-12-17 11:05:25 -0800

    Remove definitions that differ for 32 vs 64 bit table collections.

--- src/tsdef.rs
+++ src/tsdef.rs
@@ -1,10 +1,4 @@
-/// Integer type used to refer to [Node](struct.Node.html) objects.
-pub type TsInt = i32;
 /// Integer type used to refer to genomic coordinates/positions.
 pub type POSITION = i64;
-/// Equals -1 (minus one).
-/// Primary use is to indicate a null [Node](struct.Node.html) id.
-pub static NULLTSINT: TsInt = -1;
-
-/// A vector of *sample nodes*.
-pub type SamplesVec = Vec<TsInt>;
+/// Integer type used to refer to time steps.
+pub type TIME = i64;
