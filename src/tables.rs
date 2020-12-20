50e6bccf3c1a4a780ed5aecdac3514ec2e9140d4
molpopgen <krthornt@uci.edu>
2020-12-20 13:59:34 -0800

    ancestral and derived states are now Option<Vec<u8>>. Closes #34

--- src/tables.rs
+++ src/tables.rs
@@ -66,7 +66,7 @@ pub struct Edge {
 /// ancestral state of a tables::MutationRecord
 pub struct Site {
     pub position: Position,
-    pub ancestral_state: i8,
+    pub ancestral_state: Option<Vec<u8>>,
 }
 
 /// A MutationRecord is the minimal information
@@ -76,7 +76,7 @@ pub struct MutationRecord {
     pub node: IdType,
     pub key: usize,
     pub site: usize,
-    pub derived_state: i8,
+    pub derived_state: Option<Vec<u8>>,
     pub neutral: bool,
 }
 
@@ -158,7 +158,7 @@ pub fn node_table_add_row(nodes: &mut NodeTable, time: Time, deme: IdType) -> Ta
 pub fn site_table_add_row(
     sites: &mut SiteTable,
     position: Position,
-    ancestral_state: i8,
+    ancestral_state: Option<Vec<u8>>,
 ) -> TablesResult<IdType> {
     position_non_negative(position)?;
     sites.push(Site {
@@ -173,7 +173,7 @@ pub fn mutation_table_add_row(
     node: IdType,
     key: usize,
     site: usize,
-    derived_state: i8,
+    derived_state: Option<Vec<u8>>,
     neutral: bool,
 ) -> TablesResult<IdType> {
     node_non_negative(node)?;
@@ -329,7 +329,11 @@ impl TableCollection {
         edge_table_add_row(&mut self.edges_, left, right, parent, child)
     }
 
-    pub fn add_site(&mut self, position: Position, ancestral_state: i8) -> TablesResult<IdType> {
+    pub fn add_site(
+        &mut self,
+        position: Position,
+        ancestral_state: Option<Vec<u8>>,
+    ) -> TablesResult<IdType> {
         if position >= self.length_ {
             return Err(TablesError::InvalidPosition { found: position });
         }
@@ -341,7 +345,7 @@ impl TableCollection {
         node: IdType,
         key: usize,
         site: usize,
-        derived_state: i8,
+        derived_state: Option<Vec<u8>>,
         neutral: bool,
     ) -> TablesResult<IdType> {
         mutation_table_add_row(
@@ -488,5 +492,91 @@ mod test_tables {
             |x: TablesError| assert_eq!(x, TablesError::InvalidNodeValue { found: -2 }),
             |_| panic!(),
         );
+    }
+
+    #[test]
+    fn test_add_site_with_ancestral_state() {
+        let mut tables = TableCollection::new(10).unwrap();
+        let _ = tables
+            .add_site(1, Some(b"0".to_vec()))
+            .map_or_else(|_: TablesError| panic!(), |_| ());
+        let s = tables.site(0);
+        assert_eq!(s.position, 1);
+        assert_eq!(s.ancestral_state, Some(b"0".to_vec()));
+        assert_eq!(s.ancestral_state.as_ref().unwrap().len(), 1);
+
+        match std::str::from_utf8(s.ancestral_state.as_ref().unwrap()) {
+            Ok(x) => assert_eq!(x, "0".to_string()),
+            Err(_) => panic!(),
+        }
+    }
+
+    fn decode_complex_state<T>(v: &[T]) -> &[T; 4] {
+        use std::convert::TryInto;
+        v.try_into().unwrap_or_else(|_| panic!())
+    }
+
+    #[test]
+    fn test_add_site_with_complex_ancestral_state() {
+        let mut tables = TableCollection::new(10).unwrap();
+        let mut a: Vec<u8> = vec![];
+        let astate_part1: i32 = 300;
+        let astate_part2: i32 = 3;
+        a.append(&mut astate_part1.to_le_bytes().to_vec());
+        a.append(&mut astate_part2.to_le_bytes().to_vec());
+        assert_eq!(a.len(), 8);
+        let _ = tables
+            .add_site(1, Some(a))
+            .map_or_else(|_: TablesError| panic!(), |_| ());
+
+        // now, decode it
+        let s = tables.site(0);
+
+        // The conversion works by dereferencing a reference
+        // to a mutable array whose lifetime is tied to the
+        // slice:
+        let x = i32::from_le_bytes(*decode_complex_state(
+            &s.ancestral_state.as_ref().unwrap()[0..4],
+        ));
+        assert_eq!(x, astate_part1);
+        let x = i32::from_le_bytes(*decode_complex_state(
+            &s.ancestral_state.as_ref().unwrap()[4..8],
+        ));
+        assert_eq!(x, astate_part2);
+    }
+
+    #[test]
+    fn test_add_site_without_ancestral_state() {
+        let mut tables = TableCollection::new(10).unwrap();
+        let _ = tables
+            .add_site(1, None)
+            .map_or_else(|_: TablesError| panic!(), |_| ());
+        let s = tables.site(0);
+        if s.ancestral_state.as_ref().is_some() {
+            panic!()
+        }
+    }
+
+    #[test]
+    fn test_add_mutation_without_derived_state() {
+        let mut tables = TableCollection::new(10).unwrap();
+        let _ = tables.add_mutation(0, 0, 0, None, false).unwrap();
+        let m = tables.mutation(0);
+        if m.derived_state.as_ref().is_some() {
+            panic!()
+        }
+    }
+
+    #[test]
+    fn test_add_mutation_with_derived_state() {
+        let mut tables = TableCollection::new(10).unwrap();
+        let _ = tables
+            .add_mutation(0, 0, 0, Some(b"0".to_vec()), false)
+            .unwrap();
+        let m = tables.mutation(0);
+        match std::str::from_utf8(m.derived_state.as_ref().unwrap()) {
+            Ok(x) => assert_eq!(x, "0".to_string()),
+            Err(_) => panic!(),
+        }
     }
 }
