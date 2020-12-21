3571ca87bc44babf7f7133bc2a673fd5d7256b01
molpopgen <krthornt@uci.edu>
2020-12-21 10:40:29 -0800

    Implement "edge buffering" method to replace "record and sort". Closes #25

--- forrustts_examples/neutral_wf.rs
+++ forrustts_examples/neutral_wf.rs
@@ -1,5 +1,5 @@
 use clap::{value_t, value_t_or_exit, App, Arg};
-use forrustts::wright_fisher::neutral_wf;
+use forrustts::wright_fisher::*;
 
 fn main() {
     let matches = App::new("neutral_wf")
@@ -44,14 +44,33 @@ fn main() {
                 .help("Random number seed")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("buffer_edges")
+                .short("B")
+                .long("buffer_edges")
+                .help("Use edge buffering instead of sorting")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("psurvival")
+                .short("P")
+                .long("psurvival")
+                .help("Survival probability")
+                .takes_value(true),
+        )
         .get_matches();
+
+    // TODO: default params
 
     let popsize = value_t_or_exit!(matches.value_of("popsize"), u32);
     let g = value_t_or_exit!(matches.value_of("generations"), i64);
     let rho = value_t_or_exit!(matches.value_of("rho"), f64);
     let simplify_input = value_t!(matches.value_of("simplification_interval"), i64).unwrap_or(-1);
+    let psurvival = value_t!(matches.value_of("psurvival"), f64).unwrap_or(0.0);
     let seed = value_t_or_exit!(matches.value_of("seed"), usize);
     let outfile = value_t_or_exit!(matches.value_of("outfile"), String);
+
+    // TODO: parameter validation..
 
     let mut simplify: Option<i64> = None;
 
@@ -61,14 +80,26 @@ fn main() {
 
     let r = rho / (4.0 * popsize as f64);
 
-    let mut tables = neutral_wf(seed, popsize, g, 10000000, r, 0.0, simplify);
+    let flags = if matches.is_present("buffer_edges") {
+        SimulationFlags::BUFFER_EDGES
+    } else {
+        SimulationFlags::empty()
+    };
 
-    let mut is_sample = vec![0 as i32; tables.num_nodes()];
-    for (i, n) in tables.enumerate_nodes() {
-        if n.time == g {
-            is_sample[i] = 1;
-        }
-    }
+    let (mut tables, is_sample) = neutral_wf(
+        PopulationParams {
+            size: popsize,
+            genome_length: 10000000,
+            littler: r,
+            psurvival,
+        },
+        SimulationParameters {
+            simplification_interval: simplify,
+            seed,
+            nsteps: g,
+            flags,
+        },
+    );
 
     let mut tskit_tables = forrustts::tskit::convert_to_tskit_and_drain(
         &is_sample,
--- src/test_simplify_tables.rs
+++ src/test_simplify_tables.rs
@@ -5,10 +5,11 @@ mod test {
     // We'll clean this up later when we get better abstractions
     // into tskit_rust.
     use crate::simplify_tables;
-    use crate::tsdef::IdType;
-    use crate::wright_fisher::neutral_wf;
+    use crate::tsdef::{IdType, Position, Time};
+    use crate::wright_fisher::*;
     use crate::SimplificationFlags;
     use crate::SimplificationOutput;
+    use crate::TableCollection;
     use std::mem::MaybeUninit;
     use tskit_rust::bindings as tskr;
 
@@ -30,26 +31,50 @@ mod test {
         tsk_ts
     }
 
+    fn simulate_data(
+        num_generations: Time,
+        genome_length: Position,
+        psurvival: f64,
+        seed: usize,
+        simplification_interval: Option<Time>,
+        flags: SimulationFlags,
+    ) -> (TableCollection, Vec<i32>) {
+        // None here means "never simplify".
+        neutral_wf(
+            PopulationParams {
+                size: 250,
+                genome_length,
+                littler: 5e-3,
+                psurvival,
+            },
+            SimulationParameters {
+                simplification_interval,
+                seed,
+                nsteps: num_generations,
+                flags,
+            },
+        )
+    }
+
     #[test]
-    fn test_wright_fisher() {
+    fn test_kc_distance_to_tskit() {
         // 1. Simulate in rust
         // 2. Copy to tskit and reverse time
         // 3. sort and simplify via tskit
         // 4. sort and simplify via rust
         // 5. Do we get the same stuff out?
+
         let num_generations = 5000;
         let genome_length = 1000000;
 
-        // None here means "never simplify".
-        let mut tables = neutral_wf(42, 250, num_generations, genome_length, 5e-3, 0.0, None);
-
-        let mut is_sample: Vec<i32> = vec![0; tables.num_nodes()];
-
-        for (i, n) in tables.nodes().iter().enumerate() {
-            if n.time == num_generations {
-                is_sample[i] = 1;
-            }
-        }
+        let (mut tables, mut is_sample) = simulate_data(
+            num_generations,
+            genome_length,
+            0.0,
+            42,
+            None,
+            SimulationFlags::empty(),
+        );
 
         let mut tsk_tables = crate::tskit::convert_to_tskit(
             &tables,
@@ -127,6 +152,135 @@ mod test {
 
             tskr::tsk_treeseq_free(tsk_ts.as_mut_ptr());
             tskr::tsk_treeseq_free(rust_ts.as_mut_ptr());
+        }
+    }
+
+    #[test]
+    fn test_buffer_vs_sort() {
+        let num_generations = 5000;
+        let genome_length = 1000000;
+
+        let flags = SimulationFlags::USE_STATE;
+        let (tables_sorted, is_sample_sorted) = simulate_data(
+            num_generations,
+            genome_length,
+            0.0,
+            14613641,
+            Some(100),
+            flags,
+        );
+
+        let flags = SimulationFlags::BUFFER_EDGES;
+        let (tables_buffered, is_sample_buffered) = simulate_data(
+            num_generations,
+            genome_length,
+            0.0,
+            14613641,
+            Some(100),
+            flags,
+        );
+
+        // The sums of node times should be the same if we've got
+        // both methods working
+        let sum_times_sorted: Time = tables_sorted.nodes_.iter().map(|x| x.time).sum();
+        let sum_times_buffered: Time = tables_buffered.nodes_.iter().map(|x| x.time).sum();
+        assert_eq!(sum_times_sorted, sum_times_buffered);
+
+        let mut tables_sorted_tskit = crate::tskit::convert_to_tskit(
+            &tables_sorted,
+            &is_sample_sorted,
+            crate::tskit::simple_time_reverser(num_generations),
+            true,
+        );
+
+        let mut tables_buffered_tskit = crate::tskit::convert_to_tskit(
+            &tables_buffered,
+            &is_sample_buffered,
+            crate::tskit::simple_time_reverser(num_generations),
+            true,
+        );
+
+        let mut sorted_ts = tables_to_treeseq(&mut tables_sorted_tskit);
+        let mut buffered_ts = tables_to_treeseq(&mut tables_buffered_tskit);
+        unsafe {
+            assert_eq!(500, tskr::tsk_treeseq_get_num_samples(sorted_ts.as_ptr()));
+            assert_eq!(500, tskr::tsk_treeseq_get_num_samples(buffered_ts.as_ptr()));
+            let mut kc: f64 = -1.;
+            let kcp: *mut f64 = &mut kc;
+            let rv = tskr::tsk_treeseq_kc_distance(
+                sorted_ts.as_mut_ptr(),
+                buffered_ts.as_mut_ptr(),
+                0.,
+                kcp,
+            );
+            assert_eq!(rv, 0);
+            assert!((kc - 0.).abs() < f64::EPSILON);
+            tskr::tsk_treeseq_free(sorted_ts.as_mut_ptr());
+            tskr::tsk_treeseq_free(buffered_ts.as_mut_ptr());
+        }
+    }
+
+    // The KC distance code will barf on trees where samples
+    // have different ages.  So we have to use less direct methods
+    // to compare.
+    #[test]
+    fn test_buffer_vs_sort_overlapping_generations() {
+        let num_generations = 5000;
+        let genome_length = 1000000;
+
+        let flags = SimulationFlags::USE_STATE;
+        let (tables_sorted, is_sample_sorted) = simulate_data(
+            num_generations,
+            genome_length,
+            0.5,
+            14613641,
+            Some(100),
+            flags,
+        );
+
+        let flags = SimulationFlags::BUFFER_EDGES;
+        let (tables_buffered, is_sample_buffered) = simulate_data(
+            num_generations,
+            genome_length,
+            0.5,
+            14613641,
+            Some(100),
+            flags,
+        );
+
+        assert_eq!(tables_sorted.num_nodes(), tables_buffered.num_nodes());
+
+        // The sums of node times should be the same if we've got
+        // both methods working
+        let sum_times_sorted: Time = tables_sorted.nodes_.iter().map(|x| x.time).sum();
+        let sum_times_buffered: Time = tables_buffered.nodes_.iter().map(|x| x.time).sum();
+        assert_eq!(sum_times_sorted, sum_times_buffered);
+
+        let mut tables_sorted_tskit = crate::tskit::convert_to_tskit(
+            &tables_sorted,
+            &is_sample_sorted,
+            crate::tskit::simple_time_reverser(num_generations),
+            true,
+        );
+
+        let mut tables_buffered_tskit = crate::tskit::convert_to_tskit(
+            &tables_buffered,
+            &is_sample_buffered,
+            crate::tskit::simple_time_reverser(num_generations),
+            true,
+        );
+
+        let mut sorted_ts = tables_to_treeseq(&mut tables_sorted_tskit);
+        let mut buffered_ts = tables_to_treeseq(&mut tables_buffered_tskit);
+        unsafe {
+            assert_eq!(500, tskr::tsk_treeseq_get_num_samples(sorted_ts.as_ptr()));
+            assert_eq!(500, tskr::tsk_treeseq_get_num_samples(buffered_ts.as_ptr()));
+            assert_eq!(
+                tskr::tsk_treeseq_get_num_trees(sorted_ts.as_ptr()),
+                tskr::tsk_treeseq_get_num_trees(buffered_ts.as_ptr())
+            );
+            tskr::tsk_treeseq_free(sorted_ts.as_mut_ptr());
+            tskr::tsk_treeseq_free(buffered_ts.as_mut_ptr());
         }
     }
 }
