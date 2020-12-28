6491fce1e1604e06c086e0078bf73d28afc191a1
molpopgen <krthornt@uci.edu>
2020-12-28 06:29:16 -0800

    Add SimplificationFlags for table validation.

--- src/simplification_flags.rs
+++ src/simplification_flags.rs
@@ -4,19 +4,20 @@ bitflags! {
     /// Boolean flags affecting simplification
     /// behavior.
     ///
-    /// Currently, this is unused, and exists
-    /// as a placeholder for the future.
-    ///
     /// # Example
     ///
     /// ```
     /// let e = forrustts::SimplificationFlags::empty();
-    /// assert!(e.contains(forrustts::SimplificationFlags::NONE));
+    /// assert_eq!(e.bits(), 0);
     /// ```
     #[derive(Default)]
     pub struct SimplificationFlags: u32 {
-        /// Placeholder
-        const NONE = 0;
+        /// Validate that input edges are sorted
+        const VALIDATE_EDGES = 1 << 0;
+        /// Validate that input mutations are sorted
+        const VALIDATE_MUTATIONS = 1 << 1;
+        /// Validate all tables.
+        const VALIDATE_ALL = Self::VALIDATE_EDGES.bits | Self::VALIDATE_MUTATIONS.bits;
     }
 }
 
@@ -27,7 +28,6 @@ mod test {
     #[test]
     fn test_empty() {
         let e = SimplificationFlags::empty();
-        assert!(e.contains(SimplificationFlags::NONE));
         assert_eq!(e.bits(), 0);
     }
 }
