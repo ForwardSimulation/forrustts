5b261fbaa143e1176ce5ca13400a03d5dd911f6f
molpopgen <krthornt@uci.edu>
2021-09-07 14:12:50 -0700

    add forrustts-genetic-maps

---
+++ forrustts-genetic-maps/src/lib.rs
@@ -0,0 +1,17 @@
+use forrustts_rng::Rng;
+use forrustts_tables_trees::Position;
+
+pub trait GeneticMapElement {
+    fn begin(&self) -> Position;
+    fn end(&self) -> Position;
+    // Should we expect a &[Position] instead?
+    fn generate_breakpoints(&self, rng: &mut Rng, breakpoints: &mut Vec<Position>) -> ();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
