bc7da9411c32440e676120244a65770e84d35263
Kevin R. Thornton <krthornt@uci.edu>
2023-03-21 10:58:38 -0700

    add failing tests for later

--- forrustts-genetic-maps-rand/tests/tests.rs
+++ forrustts-genetic-maps-rand/tests/tests.rs
@@ -6,3 +6,13 @@ fn test_build_empty() {
     let builder = GeneticMapBuilder::default();
     assert!(GeneticMap::new_from_builder(builder).is_some());
 }
+
+#[test]
+fn test_build_non_empty() {
+    unimplemented!("test not written");
+}
+
+#[test]
+fn test_generate_breakpoints() {
+    unimplemented!("test not written");
+}
