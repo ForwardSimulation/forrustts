34084d21a1f8293f65ee9a9461521c5fab677893
Kevin R. Thornton <krthornt@uci.edu>
2023-03-21 09:17:26 -0700

    start to offload to genetic maps for core behavior

--- forrustts-genetic-maps-rand/src/lib.rs
+++ forrustts-genetic-maps-rand/src/lib.rs
@@ -2,38 +2,3 @@ use forrustts_core::newtypes::Position;
 
 use rand::Rng;
 
-pub trait CrossoverRegionModel {}
-
-pub struct BinomialRegionModel {}
-pub struct PoissonRegionModel {}
-
-impl CrossoverRegionModel for BinomialRegionModel {}
-impl CrossoverRegionModel for PoissonRegionModel {}
-
-pub struct CrossoverRegion<T>
-where
-    T: CrossoverRegionModel,
-{
-    left: Position,
-    right: Position,
-    parameter: f64,
-    marker: std::marker::PhantomData<T>,
-}
-
-impl CrossoverRegion<PoissonRegionModel> {
-    pub fn new(left: Position, right: Position, mean: f64) -> Option<Self> {
-        if left < 0 || right < 0 || right <= left || mean < 0.0 {
-            None
-        } else {
-            Some(Self {
-                left,
-                right,
-                parameter: mean,
-                marker: std::marker::PhantomData,
-            })
-        }
-    }
-}
-
-pub struct GeneticMap {}
-
--- forrustts-genetic-maps/src/lib.rs
+++ forrustts-genetic-maps/src/lib.rs
@@ -13,6 +13,81 @@ pub trait GeneticMap<T> {
     fn breakpoints(&self) -> &[Breakpoint];
 }
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct PoissonCrossover {
+    left: Position,
+    right: Position,
+    mean: f64,
+}
+
+impl PoissonCrossover {
+    pub fn new(left: Position, right: Position, mean: f64) -> Option<Self> {
+        if left < 0 || right < 0 || right <= left || !mean.is_finite() || mean < 0.0 {
+            None
+        } else {
+            Some(Self { left, right, mean })
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct BinomialCrossover {
+    left: Position,
+    right: Position,
+    probability: f64,
+}
+
+impl BinomialCrossover {
+    pub fn new(left: Position, right: Position, probability: f64) -> Option<Self> {
+        if left < 0
+            || right < 0
+            || right <= left
+            || !probability.is_finite()
+            || !(0.0..=1.0).contains(&probability)
+        {
+            None
+        } else {
+            Some(Self {
+                left,
+                right,
+                probability,
+            })
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct IndependentAssortment {
+    at: Position,
+}
+
+impl IndependentAssortment {
+    pub fn new(at: Position) -> Option<Self> {
+        if at >= 0 {
+            Some(Self { at })
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct GeneticMapBuilder {
+    poisson: Vec<PoissonCrossover>,
+    independent_assortment: Vec<IndependentAssortment>,
+}
+
+impl GeneticMapBuilder {
+    pub fn extend_poisson(mut self, intervals: &[PoissonCrossover]) -> Self {
+        self.poisson.extend_from_slice(intervals);
+        self
+    }
+    pub fn extend_independent_assortment(mut self, at: &[IndependentAssortment]) -> Self {
+        self.independent_assortment.extend_from_slice(at);
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
