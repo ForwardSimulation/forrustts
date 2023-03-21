96d77c64b537fce8b9dd5835e625d47906e5ec22
Kevin R. Thornton <krthornt@uci.edu>
2023-03-21 08:39:28 -0700

    typestate-like experiments.  may not be the right way

--- forrustts-genetic-maps-rand/src/lib.rs
+++ forrustts-genetic-maps-rand/src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use forrustts_core::newtypes::Position;
+
+use rand::Rng;
+
+pub trait CrossoverRegionModel {}
+
+pub struct BinomialRegionModel {}
+pub struct PoissonRegionModel {}
+
+impl CrossoverRegionModel for BinomialRegionModel {}
+impl CrossoverRegionModel for PoissonRegionModel {}
+
+pub struct CrossoverRegion<T>
+where
+    T: CrossoverRegionModel,
+{
+    left: Position,
+    right: Position,
+    parameter: f64,
+    marker: std::marker::PhantomData<T>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl CrossoverRegion<PoissonRegionModel> {
+    pub fn new(left: Position, right: Position, mean: f64) -> Option<Self> {
+        if left < 0 || right < 0 || right <= left || mean < 0.0 {
+            None
+        } else {
+            Some(Self {
+                left,
+                right,
+                parameter: mean,
+                marker: std::marker::PhantomData,
+            })
+        }
     }
 }
+
+pub struct GeneticMap {}
+
