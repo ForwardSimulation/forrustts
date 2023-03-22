2dbd860c6d06bf9b0a3a9c5797db7e1519ca1535
Kevin R. Thornton <krthornt@uci.edu>
2023-03-21 20:31:00 -0700

    more tests

--- forrustts-genetic-maps/tests/tests.rs
+++ forrustts-genetic-maps/tests/tests.rs
@@ -1,3 +1,4 @@
+use forrustts_genetic_maps::BernoulliCrossover;
 use forrustts_genetic_maps::GenerateBreakpoints;
 use forrustts_genetic_maps::GeneticMap;
 use forrustts_genetic_maps::GeneticMapBuilder;
@@ -18,11 +19,13 @@ fn test_build_non_empty() {
 }
 
 #[test]
-fn test_generate_breakpoints() {
+fn test_generate_bernoulli_breakpoints() {
     let builder = GeneticMapBuilder::default()
-        .extend_bernoullil(&[forrustts_genetic_maps::BernoulliCrossover::new(0, 1, 1.).unwrap()]);
+        .extend_bernoullil(&[BernoulliCrossover::new(10, 20, 1.).unwrap()])
+        .extend_bernoullil(&[BernoulliCrossover::new(0, 10, 1.).unwrap()]);
     let mut map = GeneticMap::new_from_builder(builder).unwrap();
     let mut rng = rand::rngs::StdRng::seed_from_u64(0);
     map.generate_breakpoints(&mut rng);
-    assert_eq!(map.breakpoints().len(), 1);
+    assert_eq!(map.breakpoints().len(), 2);
+    assert!(map.breakpoints().windows(2).all(|w| { w[0] <= w[1] }));
 }
