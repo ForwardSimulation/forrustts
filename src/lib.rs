eacf942f31190e866096a95e3cef8d557c5c4381
Kevin R. Thornton <krthornt@uci.edu>
2023-04-05 14:23:26 -0700

    refactor!: delete crate that we are not willing to commit to yet

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,6 @@
 //! In the `examples/` directory of the project repository.
 
 pub use forrustts_core::newtypes::*;
-pub use forrustts_simplification::*;
-pub use forrustts_tables::*;
-pub use forrustts_trees::*;
-
-pub mod tskit_export;
 
 /// Get the forrustts version number.
 pub fn version() -> &'static str {
