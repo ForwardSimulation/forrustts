f1f41d5d089b377f92b64489fe42b0497abbe63e
Kevin R. Thornton <krthornt@uci.edu>
2023-04-06 11:16:50 -0700

    test: add tests of Uniform::<Position>::new_inclusive

--- forrustts-core/tests/tests.rs
+++ forrustts-core/tests/tests.rs
@@ -19,4 +19,20 @@ mod test_rand_traits {
             }
         }
     }
+
+    proptest! {
+        #[test]
+        fn test_uniform_position_inclusive(a in 0..i64::MAX, b in 0..i64::MAX,
+                                           seed in 0..u64::MAX) {
+            if a != b { // else rand will panic
+                let lo = Position::new_valid(std::cmp::min(a, b));
+                let hi = Position::new_valid(std::cmp::max(a, b));
+                let upos = rand::distributions::Uniform::<Position>::new_inclusive(lo, hi);
+                let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
+                for _ in 0..100 {
+                    let _ = rng.sample(upos);
+                }
+            }
+        }
+    }
 }
